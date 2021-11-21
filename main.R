9b12d582e74a752948f64dca7b5d3110fd4aa94e
Przemysław Pająk <office@fearlessspider.com>
2021-11-22 00:18:08 +0100

    Initial

---
+++ main.R
@@ -0,0 +1,5 @@
+library(tidyverse)  # for grammar
+library(scales)     # for pretty breaks in figures
+library(tidyquant)  # for data download
+library(knitr)      # for html knitting
+library(kableExtra) # for nicer tables
